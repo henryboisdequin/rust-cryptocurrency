c4319f581f008dc4958a2b24e3f52dc02d2113d3
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2021-01-10 19:35:32 +0530

    adding more than 1 block

--- src/main.rs
+++ src/main.rs
@@ -34,33 +34,31 @@ fn main() {
         .update_with_block(genesis_block)
         .expect("Failed to add genesis block.");
 
-    let mut block = Block::new(
-        1,
-        now(),
-        last_hash,
-        vec![Transaction {
-            inputs: vec![],
-            outputs: vec![
-                transaction::Output {
-                    to_addr: "Henry".to_owned(),
-                    value: 50,
-                },
-                transaction::Output {
-                    to_addr: "Bob".to_owned(),
-                    value: 20,
-                },
-            ],
-        }],
-        difficulty,
-    );
-
-    block.mine();
-
-    println!("Mined genesis block: {:?}", &block);
-
-    last_hash = block.hash.clone();
-
-    blockchain
-        .update_with_block(block)
-        .expect("Failed to add block.");
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i,
+            now(),
+            last_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Henry".to_owned(),
+                        value: 50,
+                    },
+                    transaction::Output {
+                        to_addr: "Bob".to_owned(),
+                        value: 20,
+                    },
+                ],
+            }],
+            difficulty,
+        );
+        block.mine();
+        println!("Mined genesis block: {:?}", &block);
+        last_hash = block.hash.clone();
+        blockchain
+            .update_with_block(block)
+            .expect("Failed to add block.");
+    }
 }
