8e0f9ec86c862dc4250501ea4147124815fe4a50
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2021-01-10 17:14:26 +0530

    verify tests

---
+++ src/blockchain.rs
@@ -0,0 +1,37 @@
+use super::*;
+
+pub struct Blockchain {
+    pub blocks: Vec<Block>,
+}
+
+impl Blockchain {
+    pub fn verify(&self) -> bool {
+        for (i, block) in self.blocks.iter().enumerate() {
+            if block.index != i as u32 {
+                println!("Index mismatch {} != {}", &block.index, &i);
+                return false;
+            } else if !check_difficulty(&block.hash(), block.difficulty) {
+                println!("Difficulty failed");
+                return false;
+            } else if i != 0 {
+                // not genesis block
+                let prev_block = &self.blocks[i - 1];
+                if block.timestamp <= prev_block.timestamp {
+                    println!("Time didn't increase");
+                    return false;
+                } else if block.prev_block_hash != prev_block.hash {
+                    println!("Hash mismatch");
+                    return false;
+                }
+            } else {
+                // genesis block
+                if block.prev_block_hash != vec![0; 32] {
+                    println!("Genesis block prev_block_hash invalid");
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
 use blockchainlib::*;
 
 fn main() {
+    let difficulty = 0x006fffffffffffffffffffffffffff;
     let mut block = Block::new(
         0,
-        0,
+        now(),
         vec![0; 32],
         0,
         "Genesis block".to_owned(),
-        0x000000ffffffffffffffffffffffffff,
+        difficulty,
     );
 
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
-
     block.mine();
+    println!("Mined genesis block: {:?}", &block);
 
-    println!("{:?}", &block);
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    println!("Verify: {}", &blockchain.verify());
+
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i,
+            now(),
+            last_hash,
+            0,
+            "Another block".to_owned(),
+            difficulty,
+        );
+
+        block.mine();
+        println!("Mined block: {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+
+        println!("Verify: {}", &blockchain.verify());
+    }
 }
