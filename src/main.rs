3c2ab20ded2dcc66851f2f43d243ea3ce1df5ef9
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2021-01-10 16:23:00 +0530

    added mining

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+    let mut block = Block::new(
+        0,
+        0,
+        vec![0; 32],
+        0,
+        "Genesis block".to_owned(),
+        0x000000ffffffffffffffffffffffffff,
+    );
 
-    let hash = block.hash();
+    block.hash = block.hash();
 
-    block.hash = hash;
+    println!("{:?}", &block);
+
+    block.mine();
+
     println!("{:?}", &block);
 }
